8210e775367615fa3b788c6f975a9147bbfb0207
alexey.zh <mailbox.stdio@gmail.com>
2025-01-15 17:08:27 +0500

    fields

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/kube-openapi/pkg/util/proto"
+	"k8s.io/kubectl/pkg/explain"
+	"k8s.io/kubectl/pkg/util/openapi"
+
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
 type TreeDataNodeType string
@@ -20,6 +28,7 @@ var (
 	nodeTypeRoot     TreeDataNodeType = "root"
 	nodeTypeGroup    TreeDataNodeType = "group"
 	nodeTypeResource TreeDataNodeType = "resource"
+	nodeTypeField    TreeDataNodeType = "field"
 )
 
 type TreeData struct {
@@ -28,6 +37,8 @@ type TreeData struct {
 	// it means, that node is already opened in sub-view
 	// do not add it to a stack view again and again
 	inPreview bool
+
+	originalPath string
 }
 
 // TODO: cleanup
@@ -73,6 +84,10 @@ func customSortGroups(groups []*metav1.APIResourceList) {
 	})
 }
 
+func defaultConfigFlags() *genericclioptions.ConfigFlags {
+	return genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag().WithDiscoveryBurst(300).WithDiscoveryQPS(50.0)
+}
+
 func main() {
 	// Load the kubeconfig file
 	kubeconfig := os.Getenv("KUBECONFIG")
@@ -89,6 +104,25 @@ func main() {
 
 	// Get the discovery client
 	discoveryClient := clientset.Discovery()
+
+	// flags+
+	ioStreams := genericclioptions.IOStreams{
+		In:     os.Stdin,
+		Out:    os.Stdout,
+		ErrOut: os.Stderr,
+	}
+	kubeConfigFlags := defaultConfigFlags().WithWarningPrinter(ioStreams)
+	matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubeConfigFlags)
+	f := cmdutil.NewFactory(matchVersionKubeConfigFlags)
+	restMapper, err := f.ToRESTMapper()
+	if err != nil {
+		log.Fatal(err)
+	}
+	openApiSchema, err := f.OpenAPISchema()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// flags-
 
 	// Get API resources
 	resources, err := discoveryClient.ServerPreferredResources()
@@ -139,6 +173,16 @@ func main() {
 			resourceNode := tview.NewTreeNode(fmt.Sprintf("%s (%s)", resource.Kind, resource.Name)).
 				SetColor(tcell.ColorWhite).
 				SetReference(data)
+
+			// fields+
+			paths := getPaths(restMapper, openApiSchema, gvr)
+			rootFieldsNode := &Node{Name: "root"}
+			for _, line := range paths {
+				rootFieldsNode.AddPath(line.original)
+			}
+			addChildrenFields(resourceNode, rootFieldsNode.Children)
+			// fields-
+
 			groupNode.AddChild(resourceNode)
 		}
 
@@ -246,5 +290,164 @@ func main() {
 
 	if err := app.SetRoot(layout, true).Run(); err != nil {
 		panic(err)
+	}
+}
+
+func addChildrenFields(parent *tview.TreeNode, children map[string]*Node) {
+	if len(children) != 0 {
+		parent.SetColor(tcell.ColorGreen)
+		parent.SetExpanded(!parent.IsExpanded())
+	}
+
+	keys := make([]string, 0, len(children))
+	for key := range children {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		childNode := tview.NewTreeNode(children[key].Name).SetReference(&TreeData{
+			nodeType:     nodeTypeField,
+			originalPath: children[key].OriginalPath,
+		})
+		parent.AddChild(childNode)
+		if children[key].Children != nil {
+			addChildrenFields(childNode, children[key].Children)
+		}
+	}
+}
+
+func getPaths(restMapper meta.RESTMapper, openApiSchema openapi.Resources, gvr schema.GroupVersionResource) []path {
+	var paths []path
+	visitor := &schemaVisitor{
+		pathSchema: make(map[path]proto.Schema),
+		prevPath: path{
+			original: strings.ToLower(gvr.Resource),
+		},
+		err: nil,
+	}
+	gvk, err := restMapper.KindFor(gvr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := openApiSchema.LookupResource(gvk)
+	if s == nil {
+		log.Fatal(err)
+	}
+	s.Accept(visitor)
+	if visitor.err != nil {
+		log.Fatal(visitor.err)
+	}
+	for _, p := range visitor.listPaths() {
+		paths = append(paths, p)
+	}
+	return paths
+}
+
+//////////////////////////////////////////////////////////////////////
+// schema visitor
+
+type path struct {
+	original string
+}
+
+type schemaVisitor struct {
+	prevPath   path
+	pathSchema map[path]proto.Schema
+	err        error
+}
+
+var _ proto.SchemaVisitor = (*schemaVisitor)(nil)
+
+func (v *schemaVisitor) VisitKind(k *proto.Kind) {
+	keys := k.Keys()
+	paths := make([]path, len(keys))
+	for i, key := range keys {
+		paths[i] = path{
+			original: strings.Join([]string{v.prevPath.original, key}, "."),
+		}
+	}
+	for i, key := range keys {
+		schema, err := explain.LookupSchemaForField(k, []string{key})
+		if err != nil {
+			v.err = err
+			return
+		}
+		// if _, ok := schema.(*proto.Array); ok {
+		// 	// TODO: types for print on UI?
+		// }
+		v.pathSchema[paths[i]] = schema
+		v.prevPath = paths[i]
+		schema.Accept(v)
+	}
+}
+
+var visitedReferences = map[string]struct{}{}
+
+func (v *schemaVisitor) VisitReference(r proto.Reference) {
+	if _, ok := visitedReferences[r.Reference()]; ok {
+		return
+	}
+	visitedReferences[r.Reference()] = struct{}{}
+	r.SubSchema().Accept(v)
+	delete(visitedReferences, r.Reference())
+}
+
+func (*schemaVisitor) VisitPrimitive(*proto.Primitive) {
+	// Nothing to do.
+}
+
+func (v *schemaVisitor) VisitArray(a *proto.Array) {
+	a.SubType.Accept(v)
+}
+
+func (v *schemaVisitor) VisitMap(m *proto.Map) {
+	m.SubType.Accept(v)
+}
+
+func (v *schemaVisitor) listPaths() []path {
+	paths := make([]path, 0, len(v.pathSchema))
+	for path := range v.pathSchema {
+		paths = append(paths, path)
+	}
+	sort.SliceStable(paths, func(i, j int) bool {
+		return paths[i].original < paths[j].original
+	})
+	return paths
+}
+
+//////////////////////////////////////////////////////////////////////
+// node
+
+type Node struct {
+	Name         string
+	Children     map[string]*Node
+	OriginalPath string
+}
+
+func NewTreeNode() *Node {
+	return &Node{
+		Children: make(map[string]*Node),
+	}
+}
+
+func (node *Node) AddPath(path string) {
+	parts := strings.Split(path, ".")
+	current := node
+	for i, part := range parts {
+
+		// Ensure the child exists
+		if current.Children == nil {
+			current.Children = make(map[string]*Node)
+		}
+
+		if _, exists := current.Children[part]; !exists {
+			current.Children[part] = NewTreeNode()
+			current.Children[part].Name = part
+		}
+		current = current.Children[part]
+		if current.OriginalPath == "" {
+			current.OriginalPath = strings.Join(parts[:i+1], ".")
+		}
 	}
 }
