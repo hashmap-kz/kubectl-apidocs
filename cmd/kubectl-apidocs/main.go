d435b9d39db04f4ad2be716580a2834b6214a7fe
alexey.zh <mailbox.stdio@gmail.com>
2025-02-26 09:45:27 +0500

    perf - logging is optional

--- cmd/kubectl-apidocs/main.go
+++ cmd/kubectl-apidocs/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/hashmap-kz/kubectl-apidocs/pkg/apidocs"
 
@@ -14,15 +16,21 @@ import (
 
 func main() {
 	// debug logger
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
-	if err != nil {
-		slog.Error("Failed to open log file", "error", err)
-		return
+	if os.Getenv("KUBECTL_APIDOCS_DEBUG_LOG") == "enable" {
+		ts := time.Now().Format("20060102150405")
+		lg := fmt.Sprintf("%s-kubectl-apidocs.log", ts)
+		file, err := os.OpenFile(lg, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+		if err != nil {
+			slog.Error("failed to open log file", "error", err)
+			return
+		}
+		defer file.Close()
+		slog.SetDefault(apidocs.InitLogger(file))
+	} else {
+		slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
 	}
-	defer file.Close()
-	logger := apidocs.InitLogger(file)
-	slog.SetDefault(logger)
 
+	// app
 	if err := cmd.NewCmdAPIDocs().Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
